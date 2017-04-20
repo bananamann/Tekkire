8cb48a29118370a6d84fc7d78a5b00f85981b904
Pierce <test@test.com>
2017-04-19 22:06:17 -0400

    fixed borked directories

---
+++ Tekkire/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tekkire.Startup))]
+namespace Tekkire
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
